d7d1691b8dceb3a7483bf7d878fd229270768b72
Denzel <denzelabad98@gmail.com>
2022-03-12 02:01:32 +1100

    Finalise webscraper

--- IMDB_WebScrape.py
+++ IMDB_WebScrape.py
@@ -11,11 +11,9 @@ import requests
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup as bs
-
-ID = ['tt14039582', 'tt11252248']
+ID = []
 review = []
 rating = []
-"""
 # Get IDs of the current top 100 Most Popular movies
 URL_List = "https://www.imdb.com/chart/moviemeter?sort=rk,asc&mode=simple&page=1"
 page = requests.get(URL_List)
@@ -25,15 +23,12 @@ movies = lister_list.find_all(class_ = 'watchlistColumn')
 for i in movies:
     x = i.findChildren(recursive = False)
     ID.append(str(x[0].get('data-tconst')))
-"""
 sel_path = "chromedriver.exe"
 options = Options()
 options.add_argument("--headless")
 driver = webdriver.Chrome(sel_path, options = options)
-
 for code in ID:
     # Begin scraping through the webpage of each movie
-    
     URL = "https://www.imdb.com/title/" + code + "/reviews?ref_=tt_urv"
     page2 = requests.get(URL)
     soup2 = bs(page2.content, "html.parser")
@@ -44,14 +39,15 @@ for code in ID:
     # Separate text from total and convert to int
     total = int(header.split(' ')[0].replace(',', ''))
     total_clicks = total//25 # Total number of clicks until the full page is loaded
-    
-    for i in range(total_clicks):
-        load = driver.find_element_by_id('load-more-trigger')
-        load.click()
-        time.sleep(4)
+    try:
+        for i in range(total_clicks):
+            load = driver.find_element_by_id('load-more-trigger')
+            load.click()
+            time.sleep(4)
+    except:
+        break
     full = bs(driver.page_source, "html.parser") # Open full page with beautifulsoup
     review_container = full.find_all(class_ = 'review-container')
-    
     for i in review_container:
         # Add ratings
         parent = i.find('span', class_ = 'rating-other-user-rating')
@@ -67,11 +63,9 @@ for code in ID:
             review.append(y.text)
         else:
             review.append(x.text)
-            
-    time.sleep(3)
-
+    time.sleep(10)
 # Aggregate into csv file
 raw_reviews = pd.DataFrame({
     'Review': review,
     'Rating': rating})
-raw_reviews.to_csv('raw_reviews_IMDB.csv')
+raw_reviews.to_csv('raw_reviews_IMDB2.csv')
